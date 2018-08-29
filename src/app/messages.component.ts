9e80cbbc3f9e6ac81d1e5098b6af92eced4e19b4
Jakkavat (Bank) Deemak <jd@manaosoftware.com>
2018-08-29 14:12:20 +0700

    Display loop data.

--- src/app/messages.component.ts
+++ src/app/messages.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'message',
-    template: 'this is the message component'
+    template: 'this is the message component <div *ngFor="let message of messages"> {{message.text}} by: {{message.owner}} </div>'
 })
 
 export class MessageCoponent{
-
+    messages = [{text:'some text',owner:'Tim'},{text:'Anther text',owner:'Bank'}];
 }
